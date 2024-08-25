4febf5626ee237239baf45cc21993ac72bf6fc82
ThiagoFragaK <thiagofraga@proton.me>
2024-08-25 12:06:06 -0300

    Add: projects routes

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProjectsController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::controller(ProjectsController::class)
+    ->prefix("projects")
+    ->group(function() {
+        Route::get('/', 'index');
+        Route::post('/create', 'store');
+        Route::put('/edit', 'edit');
+        Route::delete('/destroy', 'delete');
+    });
